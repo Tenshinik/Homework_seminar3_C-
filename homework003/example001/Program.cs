755e48660dc2bcfcfda7e745f12a51c20eaa9be4
Tenshinik <@tenshinik@gmail.com>
2022-12-06 20:23:51 +0800

    Homework example001 project

---
+++ homework003/example001/Program.cs
@@ -0,0 +1,22 @@
+﻿/* 
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+
+Console.WriteLine("Enter a five-digit number ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int temp = number;
+int number2 = 0;
+
+while(temp > 0)
+{
+    number2 = number2 * 10;
+    number2 = number2 + (temp % 10);  
+    temp = temp / 10;
+}
+Console.WriteLine(number == number2);

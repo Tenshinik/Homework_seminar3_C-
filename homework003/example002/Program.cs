3be47df31f0758630a26ca4513e70d67046e90bf
Tenshinik <@tenshinik@gmail.com>
2022-12-06 20:31:27 +0800

    Homework003 example002 project

---
+++ homework003/example002/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+A (0,0); B (1,1), -> ~1.4
+A (2,4); B (0,7) -> ~3.6
+*/
+
+Console.WriteLine("Введите координаты двух точек");
+
+int xa = Convert.ToInt32(Console.ReadLine());
+int ya = Convert.ToInt32(Console.ReadLine());
+int xb = Convert.ToInt32(Console.ReadLine());
+int yb = Convert.ToInt32(Console.ReadLine());
+
+
+double ab = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2));
+
+Console.WriteLine(ab);
+
